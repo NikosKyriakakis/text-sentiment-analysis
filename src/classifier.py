Patched cross entropy and softmax bug

src/classifier.py
@@ -4,7 +4,9 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.metrics import f1_score
-    
+
+plt.style.use('ggplot')
+
 
 class MLP(nn.Module):
     def __init__(self, args):
@@ -12,8 +14,6 @@ class MLP(nn.Module):
 
         self._topology = None
         self._args = args
-        self._optimizer = None
-        self._criterion = None
         self._logs = {
             "train_loss": [],
             "train_acc": [],
@@ -24,46 +24,68 @@ class MLP(nn.Module):
         }
 
     def setup(self):
-        if self._args.binary_class == True:
-            self._out_activation = torch.sigmoid
-        else:
-            self._out_activation = torch.softmax
-        self.configure_optimizer(self._args.optimizer)
-        self.configure_loss_function(self._args.criterion)
+        self.__configure_loss_function(self._args.criterion)
+        self.__configure_optimizer(self._args.optimizer)
+        self.__configure_metrics_method(self._args.binary_class)
 
-    def configure_loss_function(self, criterion):
+    def __configure_loss_function(self, criterion):
         if criterion == "bce_logits":
             self._criterion = nn.BCEWithLogitsLoss()
         else:
             self._criterion = nn.CrossEntropyLoss()
 
-    def configure_optimizer(self, optimizer):
+    def __configure_optimizer(self, optimizer):
         if optimizer == "Adam":
             self._optimizer = optim.Adam(self.parameters(), lr=self._args.learning_rate)
         else:
             self._optimizer = optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
+
+    def __configure_metrics_method(self, binary):
+        if binary:
+            self._compute_accuracy = self._compute_bin_accuracy
+            self._compute_f1 = self._compute_bin_f1
+            self._prepare_lossfunc = self._prepare_bin_lossfunc
+        else:
+            self._compute_accuracy = self._compute_mult_accuracy
+            self._compute_f1 = self._compute_mult_f1
+            self._prepare_lossfunc = self._prepare_mult_lossfunc
 
     def forward(self, x):
         for layer in self._topology:
             x = layer(x)
         return x
 
-    def compute_accuracy(self, y_pred, y_target):
+    def _compute_bin_accuracy(self, y_pred, y_target):
         y_target = y_target.cpu()
-        y_pred_indices = (self._out_activation(y_pred) > 0.5).cpu().long()
+        y_pred_indices = (torch.sigmoid(y_pred) > 0.5).cpu().long()
         n_correct = torch.eq(y_pred_indices, y_target).sum().item()
         return n_correct / len(y_pred_indices) * 100
 
-    def compute_f1(self, y_pred, y_target):
+    def _compute_bin_f1(self, y_pred, y_target):
         y_target = y_target.cpu()
-        y_pred_indices = (self._out_activation(y_pred) > 0.5).cpu().long()
-        return f1_score(y_target, y_pred_indices) * 100
+        y_pred_indices = (torch.sigmoid(y_pred) > 0.5).cpu().long()
+        return f1_score(y_target, y_pred_indices, average='macro') * 100
+
+    def _compute_mult_accuracy(self, y_pred, y_target):
+        _, y_pred_indices = y_pred.max(dim=1)
+        n_correct = torch.eq(y_pred_indices, y_target).sum().item()
+        return n_correct / len(y_pred_indices) * 100
+
+    def _compute_mult_f1(self, y_pred, y_target):
+        _, y_pred_indices = y_pred.max(dim=1)
+        return f1_score(y_target, y_pred_indices, average='macro') * 100
 
     def checkpoint(self):
         size = len(self._logs["val_loss"])
         if size > 1:
             if self._logs["val_loss"][size - 1] < self._logs["val_loss"][size - 2]:
                 torch.save(self.state_dict(), self._args.save_dir)
+
+    def _prepare_bin_lossfunc(self, tensor_data):
+        return tensor_data.view(-1, 1).float()
+
+    def _prepare_mult_lossfunc(self, tensor_data):
+        return tensor_data.long()
 
     def fit(self):
         # Send model to available hardware
@@ -102,17 +124,17 @@ class MLP(nn.Module):
             logits = self(x=batch_dict['x_data'].float())
 
             # Compute the loss
-            target = batch_dict['y_target'].view(-1, 1).float()
+            target = self._prepare_lossfunc(batch_dict['y_target'])
             loss = self._criterion(logits, target)
             batch_loss = loss.to("cpu").item()
             running_loss += (batch_loss - running_loss) / (batch_index + 1)
             
             # Compute the accuracy
-            batch_acc = self.compute_accuracy(logits, target)
+            batch_acc = self._compute_accuracy(logits, target)
             running_acc += (batch_acc - running_acc) / (batch_index + 1)
 
             # Compute F1-score
-            batch_f1 = self.compute_f1(logits, target)
+            batch_f1 = self._compute_f1(logits, target)
             running_f1 += (batch_f1 - running_f1) / (batch_index + 1)
 
         return running_loss, running_acc, running_f1
@@ -139,7 +161,7 @@ class MLP(nn.Module):
             logits = self(x=batch_dict['x_data'].float())
 
             # Compute the loss for that pass
-            target = batch_dict['y_target'].view(-1, 1).float()
+            target = self._prepare_lossfunc(batch_dict['y_target'])
             loss = self._criterion(logits, target)
             batch_loss = loss.to("cpu").item()
             running_loss += (batch_loss - running_loss) / (batch_index + 1)
@@ -150,10 +172,10 @@ class MLP(nn.Module):
             # Use the optimizer to take gradient step
             self._optimizer.step()
 
-            batch_acc = self.compute_accuracy(logits, target)
+            batch_acc = self._compute_accuracy(logits, target)
             running_acc += (batch_acc - running_acc) / (batch_index + 1)
 
-            batch_f1 = self.compute_f1(logits, target)
+            batch_f1 = self._compute_f1(logits, target)
             running_f1 += (batch_f1 - running_f1) / (batch_index + 1)
 
         return running_loss, running_acc, running_f1
@@ -186,21 +208,9 @@ class BOWClassifier(MLP):
         super().__init__(args)
 
         self._topology = nn.Sequential (
-            # nn.Linear(self._args.in_features, 128),
-            # nn.ReLU(),
-            # nn.Dropout(0.5),
-            # nn.Linear(128, 1)
-            nn.Linear(self._args.in_features, 1)
+            nn.Linear(self._args.in_features, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, self._args.out_units)
         )
-
-# class LSTMClassifier(MLP):
-#     def __init__(self, args):
-#         super.__init__(args)
-
-#         self.topology = nn.Sequential (
-
-#         )
-
-#     def forward(self, x):
-#         h_0 = torch.zeros(1, x.size(0), )
         